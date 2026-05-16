ce71b9939353e1deb8e460d1ff542948ab114a39
Kaironst <pedronovak4@gmail.com>
2026-05-16 19:46:14 -0300

    trocado getFirstProduct por FetchProductById em productService.ts

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,9 +1,12 @@
 import type { Product } from '../types/product.types.ts';
 
-export const getFirstProduct = async (): Promise<Product> => {
-  const response = await fetch('https://dummyjson.com/products/1');
+export const fetchProductById = async (id: number): Promise<Product | null> => {
+  const response = await fetch(`https://dummyjson.com/products/${id}`);
+  if (response.status === 404) {
+    return null;
+  }
   if (!response.ok) {
     throw new Error('Falha ao buscar produto');
   }
   return response.json();
-};
+}
