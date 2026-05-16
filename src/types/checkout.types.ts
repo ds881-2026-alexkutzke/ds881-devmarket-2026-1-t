304705e0ff4588b53511b4380d44297e980de947
Kaironst <pedronovak4@gmail.com>
2026-05-16 18:49:39 -0300

    criado checkout.types.ts como requisitado

---
+++ src/types/checkout.types.ts
@@ -0,0 +1,14 @@
+export interface BuyerInfo {
+  name: string;
+  email: string;
+  cpf: string;
+}
+
+export interface AddressInfo {
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  cidade: string;
+  uf: string,
+  numero: string
+}
