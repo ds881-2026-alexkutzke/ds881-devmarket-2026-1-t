409c35d4855da4b64ad79703af40f1c68cafa576
Kaironst <pedronovak4@gmail.com>
2026-05-17 17:04:11 -0300

    feat: Criar checkout.types.ts (#78)
    * criado checkout.types.ts como requisitado
    
    * alterado nomenclatura de addressInfo para Inglês

---
+++ src/types/checkout.types.ts
@@ -0,0 +1,13 @@
+export interface BuyerInfo {
+  name: string;
+  email: string;
+  cpf: string;
+}
+
+export interface AddressInfo {
+  cep: string;
+  street: string;
+  neighborhood: string;
+  city: string;
+  state: string,
+}
