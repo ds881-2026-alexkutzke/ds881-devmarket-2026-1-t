8bf31c5e064b57674fe7f0243a4409216a8f8759
Leticia Sanches Silva <leticiasancheslion@gmail.com>
2026-05-18 10:29:46 -0300

    feat: add fetchProducts e fetchCategories no productService.ts

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -10,3 +10,21 @@ export const fetchProductById = async (id: number): Promise<Product | null> => {
   }
   return response.json();
 }
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  const response = await fetch('https://dummyjson.com/products?limit=100');
+  if (!response.ok){
+    throw new Error('Falha ao buscar a lista de produtos');
+  }
+  const data = await response.json();
+  return data.products;
+}
+
+export const fetchCategories = async (): Promise<string[]> => {
+  const response = await fetch('https://dummyjson.com/products/categories');
+  if(!response.ok){
+    throw new Error ('Falha ao buscar categorias');
+  }
+  const data: Array<{ slug: string}> = await response.json();
+  return data.map((category) => category.slug);
+}
