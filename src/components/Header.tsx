c2c01db5c7c540eb36c7db1f93c547649d1e359c
Jamily <milyprotschnot@gmail.com>
2026-05-20 23:12:40 -0300

    fix: ajustando redimensionamento de tela

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,20 +10,26 @@ export default function Header({ cartCount = 0 }: HeaderProps) {
     <header className="header">
       <span className="header__logo">DevMarket</span>
 
-      <nav className="header__nav">
-        {/* Adicione aqui a rota da pagina Home - por enquanto nao vai pra lugar nenhum */}
-        <a href="/" className="header__link">Home</a>
+      <div className="header__right">
+        <nav className="header__nav">
+          {/* Adicione aqui a rota da pagina Home - por enquanto nao vai pra lugar nenhum */}
+          <a href="/" className="header__link">
+            Home
+          </a>
 
-        {/* Adicione aqui a rota da pagina Sobre */}
-        <a href="/sobre" className="header__link">Sobre</a>
-      </nav>
+          {/* Adicione aqui a rota da pagina Sobre */}
+          <a href="/sobre" className="header__link">
+            Sobre
+          </a>
+        </nav>
 
-      {/* Adicione aqui a rota da pagina Carrinho */}
-      <div className="header__cart">
-        <span className="header__cart-icon"><FiShoppingCart /></span>
-        {cartCount > 0 && (
-          <span className="header__badge">{cartCount}</span>
-        )}
+        {/* Adicione aqui a rota da pagina Carrinho */}
+        <div className="header__cart">
+          <span className="header__cart-icon">
+            <FiShoppingCart />
+          </span>
+          {cartCount > 0 && <span className="header__badge">{cartCount}</span>}
+        </div>
       </div>
     </header>
   );
