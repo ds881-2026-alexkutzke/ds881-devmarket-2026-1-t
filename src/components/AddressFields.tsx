8986cef83bad706bb8c2a655817e479c729c7fd1
Gabriela Gandine <152656675+gabi-gandine@users.noreply.github.com>
2026-05-21 03:27:10 -0300

    feat: address fields (#121)

---
+++ src/components/AddressFields.tsx
@@ -0,0 +1,74 @@
+import type { AddressInfo } from "../types/checkout.types"
+import './styles/AddressFields.css';
+
+type Props = {
+    addressInfo: AddressInfo | null
+    number: string
+    onNumberChange: (value: string) => void
+}
+
+export default function AddressFields({ addressInfo, number, onNumberChange}: Props) {
+  return (
+    <div className="container">
+      <div>
+        <label htmlFor="street">Logradouro</label>
+        <input
+          type="street"
+          id="logradouro"
+          value={addressInfo?.street || ''}
+          readOnly
+          tabIndex={-1}
+          className="readOnly"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="neighborhood">Bairro</label>
+        <input
+          type="text"
+          id="neighborhood"
+          value={addressInfo?.neighborhood || ''}
+          readOnly
+          tabIndex={-1}
+          className="readOnly"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="city">Cidade</label>
+        <input
+          type="text"
+          id="city"
+          value={addressInfo?.city || ''}
+          readOnly
+          tabIndex={-1}
+          className="readOnly"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="state">UF</label>
+        <input
+          type="text"
+          id="state"
+          value={addressInfo?.state || ''}
+          readOnly
+          tabIndex={-1}
+          className="readOnly"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="number">Número</label>
+        <input
+          type="text"
+          id="number"
+          value={number}
+          onChange={(e) => onNumberChange(e.target.value)}
+          placeholder="Digite o número"
+        />
+      </div>
+    </div>
+  )
+}
+
