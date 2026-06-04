e1c8a0fae58b8c6f82ca0abb2f70b4ac27223db9
Paulo Schiochet <paulsix02@gmail.com>
2026-06-04 16:53:14 -0300

    fix: configurar base do Vite e basename do router para deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ds881-devmarket-2026-1-t/',
   plugins: [react()],
   test: {
     globals: true,
